61588f131e7b5411a37c094b40440bbe0b717563
Fernando Leguia <legu0027@algonquinlive.com>
2022-03-15 18:28:23 -0400

    setting project to start working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import http from 'http'
 import app from './app.js'
 import createDebug from 'debug'
 
-const debug = createDebug('week9:httpServer')
+const debug = createDebug('mad9124-w22-ex8-auth:httpServer')
 
 const httpServer = http.createServer(app)
 
